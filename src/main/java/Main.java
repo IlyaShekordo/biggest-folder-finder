0678245c5c04b7b9d85d6c18c76206a2d34ed021
Ilya Shekordo <27phantom03@list.ru>
2023-08-24 21:55:19 +0300

    added folders of tree

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,26 +1,16 @@
 import java.io.File;
-import java.util.Arrays;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
+
     public static void main(String[] args) {
         String folderPath = "C:/Users/user/Desktop/AE projects";
         File file = new File(folderPath);
-        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        Node root = new Node(file);
+        FolderSizeCalculator calculator = new FolderSizeCalculator(root);
         ForkJoinPool pool = new ForkJoinPool();
-        long size = pool.invoke(calculator);
-        System.out.println(size);
-        
-    }
-    public static long getFolderSize(File folder) {
-        if (folder.isFile()) {
-            return folder.length();
-        }
-        long sum = 0;
-        File[] files = folder.listFiles();
-        for(File file : files) {
-            sum += getFolderSize(file);
-        }
-        return sum;
+        pool.invoke(calculator);
+        System.out.println(root.getSize());
+
     }
 }
